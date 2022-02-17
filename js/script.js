4989d84c628f2ba1d0cfc2767c196e257aba6a02
atsi87 <atsimam@gmail.com>
2022-02-17 13:00:51 -0800

    updates

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ const questionButton = document.getElementById("questionButton");
 
 questionButton.addEventListener("click", function () {
   const ask = prompt("ask me anything");
-  const magicball = magic8(1, 20);
+  const magicball = getRandomNumbers(1, 20);
   console.log(magicball);
   answer.src = `../img/magic8ball_${magicball}.png`;
 });
 
-function magic8(min, max) {
+function getRandomNumbers(min, max) {
   const minValue = Math.ceil(min);
   const maxValue = Math.floor(max);
-  return Math.floor(Math.random() * (maxValue - minValue + 2));
+  return Math.floor(Math.random() * (maxValue - minValue + 1));
 }
