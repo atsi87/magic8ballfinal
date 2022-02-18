cd3e7461360408bcd61004e2c1fd32f88a7a3da9
atsi87 <atsimam@gmail.com>
2022-02-17 19:17:52 -0800

    updated

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ questionButton.addEventListener("click", function () {
   const ask = prompt("ask me anything");
   const magicball = getRandomIntInclusive(1, 20);
   console.log(magicball);
-  answer.src = `../img/magic8ball_${magicball}.png`;
+  answer.src = `./img/magic8ball_${magicball}.png`;
 });
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min + 1) + min); 
 }
 
